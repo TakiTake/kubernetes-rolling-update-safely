99396fa641e87617eb4932219a44bd46abbd3f7e
Takeshi Takizawa <takeshi.takizawa@rakuten.com>
2018-08-12 13:02:37 +0900

    Add readinessProbe

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+    public static final String podName = System.getenv("POD_NAME");
+
     @RequestMapping("/")
     public String index() {
-        return "hello";
+        return podName;
     }
 }
